ffa8dd312d32a0503fe34c3d22931021de80bdeb
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-04-27 18:34:18 +0200

    create directive for navbar

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -3,6 +3,20 @@
 angular.module('components.navbar', [])
   .controller('NavbarController', NavbarController);
 
-  function NavbarController($scope) {
-    $scope.date = new Date();
+  function NavbarController($scope, $mdDialog) {
+    var vm = this;
+
+    vm.showSignup = showSignup;
+    vm.showLogin = showLogin;
+    
+    ////////////////////////
+
+
+    function showSignup() {
+      
+    }
+
+    function showLogin() {
+      
+    }
   }
