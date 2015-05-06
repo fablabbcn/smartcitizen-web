614fc27314d84ec89b53abf707af62a9408e20ef
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-05-06 17:58:48 +0200

    feat(navbar): add layout and parse search data

--- src/app/components/search/search.controller.js
+++ src/app/components/search/search.controller.js
@@ -24,7 +24,29 @@ angular.module('app.components')
     function querySearch(query) {
       return search.globalSearch(query)
         .then(function(data) {
-          return data;
+          console.log(data);
+          if(data.length === 0) return data;
+          return data.map(function(object) {
+            if(object.type === 'Location') {
+            } else if(object.type === 'Device') {
+              var newObj = {
+                name: object.name,
+                location: object.city + ', ' + object.country,
+                image: ''
+              };
+              return newObj;
+            } else if(object.type === 'User') {
+              console.log('username', object.username);
+              var newObj = {
+                name: object.username,
+                location: object.city + ', ' + object.country,
+                image: object.avatar
+              };
+              console.log(newObj);
+              return newObj;
+            }
+          });
         });
     }
+
   }
