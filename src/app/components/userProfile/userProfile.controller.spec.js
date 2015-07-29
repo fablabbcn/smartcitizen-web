e0155914974671a0755ff78347bb61b692d93fab
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-07-29 12:42:59 +0200

    tests: add userProfile controller tests

--- src/app/components/userProfile/userProfile.controller.spec.js
+++ src/app/components/userProfile/userProfile.controller.spec.js
@@ -8,18 +8,15 @@ describe('Controller: User Profile', function() {
   var UserProfileController,
       scope,
       mockUserConstructor,
-      mockUserData,
+      mockUserInstance,
       stateParams,
-      mockKitsData;
+      mockKitsData,
+      mockAuthService;
 
   beforeEach(inject(function($controller, $rootScope) {
     scope = $rootScope.$new();
-    
-    mockUserConstructor = function(userData) {
-      _.extend(this, userData);
-    };
 
-    mockUserData = {
+    mockUserInstance = {
       username: 'ruben'
     };
 
@@ -30,41 +27,47 @@ describe('Controller: User Profile', function() {
     mockKitsData = {
       
     };
+
+    mockAuthService = {
+
+    };
     
     UserProfileController = $controller('UserProfileController', {
-      $scope: scope, User: mockUserConstructor, userData: mockUserData, $stateParams: stateParams,
-       kitsData: mockKitsData
+      $scope: scope, User: mockUserConstructor, userData: mockUserInstance, $stateParams: stateParams,
+       kitsData: mockKitsData, auth: mockAuthService
     });
     
     //spyOn(mockUserService, 'post').and.returnValue($q.when({}));
     //spyOn(mockAnimationService, 'blur'); 
   })); 
   describe('State', function() {
-    xit('should expose a user instance', function() {
+    it('should expose a user instance', function() {
       expect(UserProfileController.user).toBeDefined();
       expect(UserProfileController.user).toEqual(jasmine.any(Object));
-      //expect(UserProfileController.user.prototype.constructor).toEqual(mockUserConstructor);
+      expect(Object.keys(UserProfileController.user)).toEqual(['username']);
     });
-    //add kits before state load on ui-router?
-    xit('should expose a kit instance', function() {
-      //expect(UserProfileController.kits).toBeDefined();
+    it('should expose a kit instance', function() {
+      expect(UserProfileController.kits).toBeDefined();
     });
-    xit('should expose filterKits function', function() {
+    it('should expose filterKits function', function() {
       expect(UserProfileController.filterKits).toEqual(jasmine.any(Function));
     });
-    xit('should expose kits filtered', function() {
+    it('should expose kits filtered', function() {
       
     });
-    xit('should expose the status of the filter with value undefined by default', function() {
+    it('should expose the status of the filter with value undefined by default', function() {
       expect(UserProfileController.status).toBeUndefined();
     });
-    xit('should expose dropdown options', function() {
-      expect(UserProfileController.dropdownOptions).toEqual(jasmine.any(Array));
-    });
-    xit('should expose dropdown options with predefined text & values', function() {
-      expect(_.pluck(UserProfileController.dropdownOptions, 'text')).toEqual(['SET UP', 'EDIT']);
-      expect(_.pluck(UserProfileController.dropdownOptions, 'value')).toEqual(['1', '2']);
-    });
   }); 
+
+  describe('Events', function() {
+    it('should listen for the loggedIn event', function() {
+
+    });
+
+    it('should update the page according to the loggedIn event handler', function() {
+
+    });
+  })
 });
 
