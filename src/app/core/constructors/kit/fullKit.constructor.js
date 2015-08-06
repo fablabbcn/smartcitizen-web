308461bd7c308dbe87dfce072fda297050b89c41
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-08-06 15:31:26 +0200

    refactor: refactor getSensors method on kit constructor

--- src/app/core/constructors/kit/fullKit.constructor.js
+++ src/app/core/constructors/kit/fullKit.constructor.js
@@ -22,35 +22,35 @@
       FullKit.prototype.constructor = FullKit;
 
       FullKit.prototype.getSensors = function(sensorTypes, options) {
-        var parsedSensors = this.data
+        var sensors = _(this.data)
+          .chain()
           .map(function(sensor) {
             return new Sensor(sensor, sensorTypes); 
-          });
-          // .sort(function(sensorA, sensorB) {
-          //   return sensorA.id - sensorB.id;
-          // });
-
-        if(options.type === 'compare') {
-          parsedSensors.unshift({
-            name: 'NONE',
-            color: 'white',
-            id: -1
-          });
-        } 
-
-        return parsedSensors.reduce(function(acc, sensor, index, arr) {
-          if(sensor.name === 'BATTERY') {
-            arr.splice(index, 1);
-            
-            if(options.type === 'main') {
-              acc[0] = arr;              
-              acc[1] = sensor;
-            } else if(options.type === 'compare') {
-              acc = arr;
+          })
+          .tap(function(sensors) {
+            if(options.type === 'compare') {
+              sensors.unshift({
+                name: 'NONE',
+                color: 'white',
+                id: -1
+              });
             }
-          }
-          return acc;
-        }, []);
+          })
+          .value();
+          
+          return sensors.reduce(function(acc, sensor, index, arr) {
+            if(sensor.name === 'BATTERY') {
+              arr.splice(index, 1);
+              
+              if(options.type === 'main') {
+                acc[0] = arr;              
+                acc[1] = sensor;
+              } else if(options.type === 'compare') {
+                acc = arr;
+              }
+            }
+            return acc;
+          }, []);
       };
 
       return FullKit;
