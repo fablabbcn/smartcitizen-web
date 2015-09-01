0973083124f55d3940e2425a91278de6bb4c774b
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-09-01 16:33:05 +0200

    feat: add modal for store

---
+++ src/app/components/store/store.controller.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .controller('StoreController', StoreController);
+
+  StoreController.$inject = ['$scope', '$mdDialog'];
+  function StoreController($scope, $mdDialog) {
+
+    $scope.showStore = showStore;
+
+    $scope.$on('showStore', function() {
+      showStore();
+    });
+    
+    ////////////////
+
+    function showStore() {
+      $mdDialog.show({
+        hasBackdrop: true,
+        controller: 'StoreDialogController',
+        templateUrl: 'app/components/store/storeModal.html',
+        clickOutsideToClose: true
+      });
+    }
+
+  }
+})();
