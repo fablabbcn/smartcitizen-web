91132dd54c76ee572283cdc1461f6b7af6e6a98c
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-09-02 19:10:54 +0200

    feat: add new kit form

--- src/app/components/kit/newKit/newKit.controller.js
+++ src/app/components/kit/newKit/newKit.controller.js
@@ -8,6 +8,16 @@
     function NewKitController(animation) {
       var vm = this;
 
+      vm.step = 1;
+
+      vm.kit = {
+        name: undefined,
+        elevation: undefined,
+        exposure: undefined
+      };
+
+      vm.exposure = ['indoor', 'outdoor'];
+
       initialize();
 
       //////////////
