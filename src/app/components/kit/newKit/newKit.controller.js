1176de8fe65612bb1dbb715cf4eac3bb241be8ad
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-09-04 11:24:50 +0200

    feat: add map to new kit form

--- src/app/components/kit/newKit/newKit.controller.js
+++ src/app/components/kit/newKit/newKit.controller.js
@@ -4,19 +4,46 @@
   angular.module('app.components')
     .controller('NewKitController', NewKitController);
 
-    NewKitController.$inject = ['animation'];
-    function NewKitController(animation) {
+    NewKitController.$inject = ['$scope', 'animation'];
+    function NewKitController($scope, animation) {
       var vm = this;
 
       vm.step = 1;
 
-      vm.kit = {
+      // FORM INFO
+      vm.kitForm = {
         name: undefined,
         elevation: undefined,
-        exposure: undefined
+        exposure: undefined,
+        location: undefined
+      };
+      vm.exposure = ['indoor', 'outdoor'];
+
+      // MAP CONFIGURATION
+      vm.markers = {
+        main: {
+          lat: undefined,
+          lng: undefined,
+          draggable: true
+        }
       };
 
-      vm.exposure = ['indoor', 'outdoor'];
+      vm.tiles = {
+        url: 'https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.loQdtLNQ8GJkJl2LUzzxVg'
+      };
+
+      vm.defaults = {
+        scrollWheelZoom: false
+      };
+
+
+      vm.kitForm.location = {
+        lat: undefined,
+        lng: undefined,
+        zoom: 16
+      };
+
+      vm.getLocation = getLocation;
 
       initialize();
 
@@ -24,6 +51,19 @@
 
       function initialize() {
         animation.viewLoaded();
+      }
+
+      function getLocation() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          $scope.$apply(function() {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            vm.kitForm.location.lat = lat;
+            vm.kitForm.location.lng = lng;
+            vm.markers.main.lat = lat;
+            vm.markers.main.lng = lng;
+          });
+        });
       }
     }
 })();
