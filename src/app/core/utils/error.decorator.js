651ce489345d674b15271d543eb86466e8cd48f5
Pral2a <guillem@pral2a.com>
2015-10-14 19:31:21 +0200

    Improved implementation quality

--- src/app/core/utils/error.decorator.js
+++ src/app/core/utils/error.decorator.js
@@ -8,9 +8,9 @@
         airbrake.setProject('561ce288daa9031312000008', '791ff4af9dda4841b5ebd594ecad961e');
         airbrake.setHost('https://errors.smartcitizen.me');
 
-        $provide.decorator("$exceptionHandler", ['$delegate', '$window', function($delegate, $window) {
+        $provide.decorator('$exceptionHandler', ['$delegate', function($delegate, $window) {
           return function (exception, cause) {
-            exception.params = {angular_cause: cause};
+            exception.params = { angular_cause: cause };
             airbrake.notify(exception);
             $delegate(exception, cause);
           }
