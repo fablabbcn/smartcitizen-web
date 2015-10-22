648d5364f58f6ace6b0ef471a2fa0ab1dd13e2e0
Arol <arolet@gmail.com>
2015-10-22 13:56:11 -0700

    style: long lines chopped

--- src/app/components/map/map.controller.js
+++ src/app/components/map/map.controller.js
@@ -25,7 +25,10 @@
       var retinaSuffix = isRetina() ? '@2x' : '';
 
       vm.tiles = {
-        url: 'https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}'+retinaSuffix+'.png?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.loQdtLNQ8GJkJl2LUzzxVg'
+        url: 'https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}'
+          + retinaSuffix +'.png'
+          + '?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.'
+          + 'loQdtLNQ8GJkJl2LUzzxVg'
       };
       //previous tile -->'https://a.tiles.mapbox.com/v4/tomasdiez.jnbhcnb2/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.loQdtLNQ8GJkJl2LUzzxVg'
 
@@ -34,7 +37,10 @@
           osm: {
             name: 'OpenStreetMap',
             type: 'xyz',
-            url: 'https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}'+retinaSuffix+'.png?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.loQdtLNQ8GJkJl2LUzzxVg'
+            url: 'https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/'
+              + '{x}/{y}' + retinaSuffix + '.png'
+              + '?access_token=pk.eyJ1IjoidG9tYXNkaWV6IiwiYSI6ImRTd01HSGsifQ.'
+              + 'loQdtLNQ8GJkJl2LUzzxVg'
           }
         },
         overlays: {
@@ -315,7 +321,16 @@
       }
 
       function isRetina(){
-        return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod|Apple)/g.test(navigator.userAgent);
+        return ((window.matchMedia
+          && (window.matchMedia('only screen and (min-resolution: 192dpi), '
+            + 'only screen and (min-resolution: 2dppx), only screen and '
+            + '(min-resolution: 75.6dpcm)').matches
+          || window.matchMedia('only screen and (-webkit-min-device-pixel-ra'
+            + 'tio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only'
+            + ' screen and (min--moz-device-pixel-ratio: 2), only screen and '
+            + '(min-device-pixel-ratio: 2)').matches))
+          || (window.devicePixelRatio && window.devicePixelRatio >= 2))
+          && /(iPad|iPhone|iPod|Apple)/g.test(navigator.userAgent);
       }
     }
 
