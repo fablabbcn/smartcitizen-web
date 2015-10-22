c2608aacbe429255efc43f79293ebafe0f7f8f28
Arol <arolet@gmail.com>
2015-10-22 13:04:09 -0700

    fix: make welcome popup to appear every 7 days

--- src/app/components/beta/beta.controller.js
+++ src/app/components/beta/beta.controller.js
@@ -4,24 +4,29 @@
   angular.module('app.components')
     .controller('BetaController', BetaController);
 
-  BetaController.$inject = ['$scope', '$mdDialog'];
-  function BetaController($scope, $mdDialog) {
+  BetaController.$inject = ['$scope', '$mdDialog', '$window'];
+  function BetaController($scope, $mdDialog, $window) {
 
     $scope.showBeta = showBeta;
 
     $scope.$on('showBeta', function() {
       showBeta();
     });
-    
+
     ////////////////
 
     function showBeta() {
-      $mdDialog.show({
-        hasBackdrop: true,
-        controller: 'StoreDialogController',
-        templateUrl: 'app/components/beta/betaModal.html',
-        clickOutsideToClose: true
-      });
+      var lastShown = $window.localStorage.getItem('smartcitizen.beta_popup');
+      if(!lastShown || moment().diff(moment(lastShown), 'days') >= 7){
+        $mdDialog.show({
+          hasBackdrop: true,
+          controller: 'StoreDialogController',
+          templateUrl: 'app/components/beta/betaModal.html',
+          clickOutsideToClose: true
+        });
+        $window.localStorage.setItem('smartcitizen.beta_popup',
+          moment().format());
+      }
     }
 
   }
