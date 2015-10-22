16edddc0867b74ff416cf34f1a83a68151c06acc
Arol <arolet@gmail.com>
2015-10-22 13:59:02 -0700

    style: bad line endings

--- src/app/components/map/map.controller.js
+++ src/app/components/map/map.controller.js
@@ -321,16 +321,16 @@
       }
 
       function isRetina(){
-        return ((window.matchMedia
-          && (window.matchMedia('only screen and (min-resolution: 192dpi), '
-            + 'only screen and (min-resolution: 2dppx), only screen and '
-            + '(min-resolution: 75.6dpcm)').matches
-          || window.matchMedia('only screen and (-webkit-min-device-pixel-ra'
-            + 'tio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only'
-            + ' screen and (min--moz-device-pixel-ratio: 2), only screen and '
-            + '(min-device-pixel-ratio: 2)').matches))
-          || (window.devicePixelRatio && window.devicePixelRatio >= 2))
-          && /(iPad|iPhone|iPod|Apple)/g.test(navigator.userAgent);
+        return ((window.matchMedia &&
+          (window.matchMedia('only screen and (min-resolution: 192dpi), ' +
+            'only screen and (min-resolution: 2dppx), only screen and ' +
+            '(min-resolution: 75.6dpcm)').matches ||
+          window.matchMedia('only screen and (-webkit-min-device-pixel-ra' +
+            'tio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only' +
+            ' screen and (min--moz-device-pixel-ratio: 2), only screen and ' +
+            '(min-device-pixel-ratio: 2)').matches)) ||
+          (window.devicePixelRatio && window.devicePixelRatio >= 2)) &&
+          /(iPad|iPhone|iPod|Apple)/g.test(navigator.userAgent);
       }
     }
 
