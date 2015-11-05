03d951d766bcbe8bbd1561b866cc10ee437d4b7f
Arol <arolet@gmail.com>
2015-11-05 13:30:49 +0100

    refactor: changed api key view to be a directive

---
+++ src/app/components/apiKey/apiKey.controller.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+  angular.module('app.components')
+    .controller('ApiKeyController', ApiKeyController);
+
+  function ApiKeyController(){
+    var vm = this;
+
+    ///////////////
+
+    
+  }
+})();
