d07003fe4ff32b37c1c26766e06ccd2185348961
Arol <arolet@gmail.com>
2015-11-09 18:32:52 +0100

    feat: added api key copy to clipboard

--- src/app/components/apiKey/apiKey.controller.js
+++ src/app/components/apiKey/apiKey.controller.js
@@ -4,11 +4,23 @@
   angular.module('app.components')
     .controller('ApiKeyController', ApiKeyController);
 
-  function ApiKeyController(){
-    // var vm = this;
+  ApiKeyController.$inject = ['alert'];
+  function ApiKeyController(alert){
+    var vm = this;
+
+    vm.copied = copied;
+    vm.copyFail = copyFail;
 
     ///////////////
 
+    function copied(){
+      alert.success('API key copied to your clipboard.');
+    }
+
+    function copyFail(err){
+      console.log('Copy error: ', err);
+      alert.error('Oops! An error occurred copying the api key.');
+    }
 
   }
 })();
