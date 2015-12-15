b25a6870c6f38859c6ac19be8f4523910236c3c7
Arol <arolet@gmail.com>
2015-12-15 17:24:10 +0100

    hotfix(geolocation): removed ipinfo

--- src/app/core/api/geolocation.service.js
+++ src/app/core/api/geolocation.service.js
@@ -6,13 +6,8 @@
 
 	  geolocation.$inject = ['$http', '$window'];
 	  function geolocation($http, $window) {
-      var position;
-      initialize();
 
       var service = {
-        getPosition: getPosition,
-        callAPI: callAPI,
-        getPositionObj: getPositionObj,
 				grantHTML5Geolocation: grantHTML5Geolocation,
 				isHTML5GeolocationGranted: isHTML5GeolocationGranted
       };
@@ -20,28 +15,6 @@
 
       ///////////////////////////
 
-      function initialize() {
-        getPosition();
-      }
-
-      function getPosition() {
-        return callAPI().success(function(data) {
-          var arrLoc = data.loc.split(',');
-          position = {
-            lat: parseFloat(arrLoc[0]),
-            lng: parseFloat(arrLoc[1])
-          };
-          return position;
-        });
-      }
-
-      function getPositionObj() {
-        return position;
-      }
-
-      function callAPI() {
-	      return $http.jsonp('http://ipinfo.io/?callback=JSON_CALLBACK');
-      }
 
 			function grantHTML5Geolocation(){
 				$window.localStorage.setItem('smartcitizen.geolocation_granted', true);
