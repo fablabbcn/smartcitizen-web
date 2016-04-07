bb57c68349eb7400d33beb3c0471e6d2ef3708c6
hiromipaw <silvia@nopressure.co.uk>
2016-04-07 18:38:59 +0200

    Fix merge conflicts w/ master

--- src/app/components/kit/editKit/editKit.controller.js
+++ src/app/components/kit/editKit/editKit.controller.js
@@ -135,7 +135,7 @@
           /*jshint camelcase: false */
           user_tags: vm.kitForm.tags.join(',')
         };
-        
+
         if(vm.macAddress == ""){
           data.mac_address = null;
         } else if(/([0-9A-Fa-f]{2}\:){5}([0-9A-Fa-f]{2})/.test(vm.macAddress)){
@@ -159,8 +159,7 @@
                 backToProfile();
               },timewait);
             })
-          .catch(function(err) {
-<<<<<<< HEAD
+            .catch(function(err) {
               if(err.data.errors.mac_address[0] === "has already been taken") {
                 timewait = 5000;
                 alert.error('You are trying to register a kit that is already registered. Please, read <a href="http://docs.smartcitizen.me/#/start/how-do-i-register-again-my-sck">How do I register again my SCK?</a> or contact <a href="mailto:support@smartcitizen.me ">support@smartcitizen.me</a> for any questions.');
@@ -171,25 +170,8 @@
                 alert.error('There has been an error during kit set up');
                 ga('send', 'event', 'Kit', 'update failed');
               }
-            })
-          .finally(function(){
-            $timeout(function(){
-              backToProfile();
-            },timewait);
-=======
-            if(err.data.errors.mac_address[0] === "has already been taken") {
-              timewait = 5000;
-              alert.error('You are trying to register a kit that is already registered. Please, read <a href="http://docs.smartcitizen.me/#/start/how-do-i-register-again-my-sck">How do I register again my SCK?</a> or contact <a href="mailto:support@smartcitizen.me ">support@smartcitizen.me</a> for any questions.');
-              ga('send', 'event', 'Kit', 'unprocessable entity');
-            }
-            else {
-              timewait=4000;
-              alert.error('There has been an error during kit set up');
-              ga('send', 'event', 'Kit', 'update failed');
-            }
-            $timeout(function(){ },timewait);
->>>>>>> 675b46f995c2cf4945db74c4c70f10745c4fc557
-          });
+              $timeout(function(){ },timewait);
+            });
       }
 
       function openKitSetup() {
