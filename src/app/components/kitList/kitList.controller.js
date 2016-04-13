61ba1453c2f91b373c4b59424def7fa63d575a90
hiromipaw <silvia@nopressure.co.uk>
2016-04-13 14:35:11 +0200

    Delete button in dropdown

--- src/app/components/kitList/kitList.controller.js
+++ src/app/components/kitList/kitList.controller.js
@@ -5,13 +5,39 @@
     .controller('KitListController', KitListController);
 
   KitListController.$inject = ['$location', '$mdDialog', '$state','$scope',
-    '$stateParams', '$timeout', '$window','alert', 'auth', 'AuthUser', 'device',
-    'kitUtils', 'userUtils'];
+    '$stateParams', '$templateCache', '$timeout', '$window','alert', 'auth',
+    'AuthUser', 'device', 'kitUtils', 'userUtils'];
   function KitListController($location, $mdDialog, $state, $scope, $stateParams,
-    $timeout, $window, alert, auth, AuthUser, device, kitUtils, userUtils) {
+    $templateCache, $timeout, $window, alert, auth, AuthUser, device, kitUtils,
+    userUtils) {
 
     var vm = this;
     vm.removeKit = removeKit;
+
+    $templateCache.put('ngDropdowns/templates/dropdownMenu.html', [
+      '<ul class="dropdown">',
+      '<li ng-repeat="item in dropdownMenu"',
+      ' class="dropdown-item"',
+      ' dropdown-item-label="labelField"',
+      ' dropdown-menu-item="item">',
+      '</li>',
+      '</ul>'
+    ].join(''));
+
+    $templateCache.put('ngDropdowns/templates/dropdownMenuItem.html', [
+      '<li ng-class="{divider: dropdownMenuItem.divider, \'divider-label\': dropdownMenuItem.divider && dropdownMenuItem[dropdownItemLabel]}">',
+      '<md-button ng-if="dropdownMenuItem.button" ng-click="{{dropdownMenuItem.click}}" class="dropdown-item-button warn" aria-label="">{{dropdownMenuItem[dropdownItemLabel]}}</md-button>',
+      '<a href="" class="dropdown-item"',
+      ' ng-if="!dropdownMenuItem.divider && !dropdownMenuItem.button"',
+      ' ng-href="{{dropdownMenuItem.href}}"',
+      ' ng-click="selectItem()">',
+      '{{dropdownMenuItem[dropdownItemLabel]}}',
+      '</a>',
+      '<span ng-if="dropdownMenuItem.divider">',
+      '{{dropdownMenuItem[dropdownItemLabel]}}',
+      '</span>',
+      '</li>'
+    ].join(''));
 
     function removeKit(kitID) {
       var confirm = $mdDialog.confirm()
