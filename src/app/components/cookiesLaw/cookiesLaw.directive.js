61e65f44e8791fb8540763586bb2ae0bef1872aa
Viktor Smari <viktorsmari@gmail.com>
2018-02-15 17:29:45 +0100

    Rewrite cookie policy
    It was using one function to both 1. check if it should hide and 2.accept
    There is a loop issue calling the ng-hide 300 times

--- src/app/components/cookiesLaw/cookiesLaw.directive.js
+++ src/app/components/cookiesLaw/cookiesLaw.directive.js
@@ -5,44 +5,41 @@ angular.module('app.components')
   .directive('cookiesLaw', cookiesLaw);
 
 
-cookiesLaw.$inject = ['$cookies', '$document'];
+cookiesLaw.$inject = ['$cookies'];
 
-function cookiesLaw($cookies, $document) {
+function cookiesLaw($cookies) {
   return {
     template:
-      '<div class="cookies-policy_container" ng-hide="consent()">' +
+      '<div class="cookies-policy_container" ng-hide="isCookieValid()">' +
       'This site uses cookies to offer you a better experience.  ' +
-      ' <a href="" ng-click="consent(true)">Accept</a> or' +
+      ' <a href="" ng-click="acceptCookie(true)">Accept</a> or' +
       ' <a ui-sref="layout.policy">Learn More.</a> ' +
       '</div>',
     controller: function($scope) {
-      var _consent = $cookies.get('consent');
 
-      if ($cookies.get('expires') < (new Date).getTime()) {
-        //console.log('Cookie has expired!');
-        $cookies.remove('consent');
-      }else{
-        //console.log('Cookie expires could be OK or just undefined');
-        // Uncomment this to force delete the cookie
-        //$cookies.remove('consent');
+      // Helpers to debug
+      //$cookies.remove('consent');
+      //$cookies.remove('expires');
+      console.log($cookies.getAll());
+
+      $scope.isCookieValid = function() {
+        return ($cookies.get('consent') === 'true') && ($scope.isCookieAlive())
       }
 
-      //console.log('all:', $cookies.getAll());
+      $scope.isCookieAlive = function() {
+        return $cookies.get('expires') > (new Date).getTime();
+      }
 
-      $scope.consent = function(consent) {
-        if (consent === undefined) {
-          return _consent;
-        } else if (consent) {
-          //console.log('Accepting cookie...');
-          var d = new Date();
-          d.setTime(d.getTime() + (30 * 24 * 60 * 60 *1000));
-          var expires = 'expires=' + d.toUTCString();
-          $cookies.put('expires', d.getTime());
-          $cookies.put('consent', true);
-          _consent = true;
-          console.log('all after accepting:', $cookies.getAll());
-        }
+      $scope.acceptCookie = function() {
+        //console.log('Accepting cookie...');
+        var d = new Date();
+        d.setTime(d.getTime() + (30 * 24 * 60 * 60 *1000));
+        var expires = 'expires=' + d.toUTCString();
+        $cookies.put('expires', d.getTime());
+        $cookies.put('consent', true);
+        console.log('all after accepting:', $cookies.getAll());
       };
+
     }
   };
 }
