f24e525acff31bb0572fbacd09062b1a13601faa
Viktor Smari <viktorsmari@gmail.com>
2019-03-29 11:47:58 +0100

    Lodash _.pluck replaced by _.map

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -37,7 +37,7 @@ module.exports = function(options) {
       .pipe($.angularFilesort())
       .pipe(concat(function(files) {
         callback(bowerDeps.js
-          .concat(_.pluck(files, 'path'))
+          .concat(_.map(files, 'path'))
           .concat(htmlFiles)
           .concat(specFiles));
       }));
