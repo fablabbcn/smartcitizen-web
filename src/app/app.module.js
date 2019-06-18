0f7fcd7c6d8431b68dd61fb77eff683d316d5c5d
Tue M. Ngo <tue.ngominh@gmail.com>
2019-06-18 16:17:35 +0200

    Button re-styling using Font Awesome library & Custom Palette (#393)
    * Add bottom & top spacing
    
    * Fix dropdown icons
    
    * Fix icons in Compare dropdown
    
    * Remove duplicated icon
    
    * Button re-styling using Font Awesome library & Custom Palette
    
    * Fix duplicated code from merging conflicts
    
    * Fix colors of icons
    
    * Update src/app/index.scss
    
    Co-Authored-By: Viktor Smári <viktorsmari@users.noreply.github.com>
    
    * Update src/app/components/layout/layout.html
    
    Co-Authored-By: Viktor Smári <viktorsmari@users.noreply.github.com>
    
    * Update src/app/components/kit/showKit/showKit.html

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -22,10 +22,66 @@ angular.module('app', [
   'ngtweet',
   'btford.socket-io',
   'ngAnimate'
-]).config(function($mdThemingProvider){
+]).config(function($mdThemingProvider) {
+
+  $mdThemingProvider.definePalette('customGreyPalette', {
+    '50': '#ffffff',
+    '100': '#ffffff',
+    '200': '#ffffff',
+    '300': '#ffffff',
+    '400': '#fbfbfb',
+    '500': '#d4d4d4',
+    '600': '#dfdfdf',
+    '700': '#d4d4d4',
+    '800': '#c8c8c8',
+    '900': '#bbbbbb',
+    'A100': '#ffffff',
+    'A200': '#ffffff',
+    'A400': '#ffffff',
+    'A700': '#aeaeae',
+    'contrastDefaultColor': 'dark'
+  });
+
+  $mdThemingProvider.definePalette('customYellowPalette', {
+    '50': '#b08406',
+    '100': '#c89707',
+    '200': '#e1a908',
+    '300': '#f6ba0c',
+    '400': '#f7c125',
+    '500': '#f8c83d',
+    '600': '#fad66f',
+    '700': '#fbdd87',
+    '800': '#fce4a0',
+    '900': '#fcebb8',
+    'A100': '#fad66f',
+    'A200': '#f9cf56',
+    'A400': '#f8c83d',
+    'A700': '#fdf2d1'
+  });
+
+  $mdThemingProvider.definePalette('customRedPalette', {
+    '50': '#fbb6bc',
+    '100': '#f99ea6',
+    '200': '#f8858f',
+    '300': '#f76d79',
+    '400': '#f55563',
+    '500': '#F43D4D',
+    '600': '#f76d79',
+    '700': '#f55563',
+    '800': '#F43D4D',
+    '900': '#c00b1b',
+    'A100': '#fcced2',
+    'A200': '#fee6e8',
+    'A400': '#fffefe',
+    'A700': '#a80a17',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100']
+  });
+
   $mdThemingProvider.theme('default')
-    .primaryPalette('indigo')
-    .accentPalette('amber');
+    .primaryPalette('customGreyPalette')
+    .accentPalette('customYellowPalette')
+    .warnPalette('customRedPalette');
 });
 
 // Here you can define a custom Palette:
