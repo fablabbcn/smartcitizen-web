1bc5dc65819cf1d4a093df5332a3da75b22cb875
Travis CI User <travis@example.org>
2019-10-09 13:52:25 +0000

    Updates

--- scripts/app-f7fbcdb40e.js
+++ scripts/app-f7fbcdb40e.js
@@ -6976,28 +6976,25 @@ function cookiesLaw($cookies) {
       }
 
       // Helpers to debug
-      //$cookies.remove('consent');
-      //$cookies.remove('expires');
+      // You can also use `document.cookie` in the browser dev console.
       //console.log($cookies.getAll());
 
       $scope.isCookieValid = function() {
         // Use a boolean for the ng-hide, because using a function with ng-hide
-        // is considered bad practice. The digest cycle will call it multiple 
+        // is considered bad practice. The digest cycle will call it multiple
         // times, in our case around 240 times.
-        $scope.isCookieValidBool = ($cookies.get('consent') === 'true') && ($scope.isCookieAlive())
-      }
-
-      $scope.isCookieAlive = function() {
-        return $cookies.get('expires') > (new Date).getTime();
+        $scope.isCookieValidBool = ($cookies.get('consent') === 'true')
       }
 
       $scope.acceptCookie = function() {
         //console.log('Accepting cookie...');
-        var d = new Date();
-        d.setTime(d.getTime() + (30 * 24 * 60 * 60 *1000));
-        var expires = 'expires=' + d.toUTCString();
-        $cookies.put('expires', d.getTime());
-        $cookies.put('consent', true);
+        var today = new Date();
+        var expireDate = new Date(today);
+        expireDate.setMonth(today.getMonth() + 6);
+
+        $cookies.put('consent', true, {'expires' : expireDate.toUTCString()} );
+
+        // Trigger the check again, after we click
         $scope.isCookieValid();
       };
 
