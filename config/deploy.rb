6f61b11521bc3c8d8802c4b3856777b127968857
Viktor Smari <viktorsmari@gmail.com>
2020-01-10 18:12:03 +0100

    Update capistrano deploy version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.11'
+lock '3.11.2'
 
 set :application, 'smartcitizen-web'
 set :repo_url, 'git@github.com:fablabbcn/smartcitizen-web.git'
