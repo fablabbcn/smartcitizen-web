f3369823f3c3022320bdc7e1116fd09d993a01e3
Oscar Gonzalez <oscgonfer@gmail.com>
2021-11-17 17:39:29 +0100

    Minor tweak in graph area

--- src/app/components/chart/chart.directive.js
+++ src/app/components/chart/chart.directive.js
@@ -115,7 +115,7 @@
       function createChart(elem) {
         d3.select(elem).selectAll('*').remove();
 
-        margin = {top: 20, right: 12, bottom: 20, left: 40};
+        margin = {top: 20, right: 12, bottom: 20, left: 42};
         width = elem.clientWidth - margin.left - margin.right;
         height = elem.clientHeight - margin.top - margin.bottom;
 
