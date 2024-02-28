1d0b584acc9a2687afa6048d21486745c6539ef9
oscgonfer <oscgonfer@gmail.com>
2024-02-28 14:09:19 +0100

    Remove duplicated dropdown option

--- src/app/core/constants/dropdownOptionsUser.constant.js
+++ src/app/core/constants/dropdownOptionsUser.constant.js
@@ -9,7 +9,7 @@
   angular.module('app.components')
     .constant('DROPDOWN_OPTIONS_USER', [
       {divider: true, text: 'Hello,', href: './profile'},
-      {text: 'PROFILE', href: './profile'},
+      // {text: 'PROFILE', href: './profile'},
       {text: 'LOGOUT', href: './logout'}
     ]);
 })();
