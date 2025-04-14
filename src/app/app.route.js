065c551daf40d8099d216f39ec5aad4c5835c8c6
oscgonfer <oscgonfer@gmail.com>
2025-04-14 13:54:40 +0200

    Remove authentication spaghetti and delegate to rails

--- src/app/app.route.js
+++ src/app/app.route.js
@@ -8,28 +8,6 @@
       Check app.config.js to know how states are protected
     */
 
-    // belongsToUser.$inject = ['$window', '$stateParams', 'auth', 'AuthUser', 'deviceUtils', 'userUtils'];
-    // function belongsToUser($window, $stateParams, auth, AuthUser, deviceUtils, userUtils) {
-    //   console.log('belongsToUser')
-    //   if(!auth.isAuth() || !$stateParams.id) {
-    //     return false;
-    //   }
-    //   var deviceID = parseInt($stateParams.id);
-
-    //   var userData = ( auth.getCurrentUser().data ) || ($window.localStorage.getItem('smartcitizen.data') && new AuthUser( JSON.parse( $window.localStorage.getItem('smartcitizen.data') )));
-    //   var belongsToUser = deviceUtils.belongsToUser(userData.devices, deviceID);
-    //   var isAdmin = userUtils.isAdmin(userData);
-    //   return isAdmin || belongsToUser;
-    // }
-
-    // redirectNotOwner.$inject = ['belongsToUser', '$location'];
-    // function redirectNotOwner(belongsToUser, $location) {
-    //   if(!belongsToUser) {
-    //     console.error('This kit does not belong to user');
-    //     $location.path('/kits/');
-    //   }
-    // }
-
     config.$inject = ['$stateProvider', '$urlServiceProvider', '$locationProvider', 'RestangularProvider', '$logProvider', '$mdAriaProvider', '$cookiesProvider'];
     function config($stateProvider, $urlServiceProvider, $locationProvider, RestangularProvider, $logProvider, $mdAriaProvider, $cookiesProvider) {
       $stateProvider
@@ -85,18 +63,13 @@
         -- Static page template --
         Template for creating other static pages.
         */
-        // .state('layout.static', {
-        //   url: '/static',
-        //   templateUrl: 'app/components/static/static.html',
-        //   controller: 'StaticController',
-        //   controllerAs: 'vm'
-        // })
         .state('layout.policy', {
           url: '/policy',
           templateUrl: 'app/components/static/policy.html',
           controller: 'StaticController',
           controllerAs: 'vm'
         })
+        // TODO redirect
         .state('layout.about', {
           url: '/about',
           templateUrl: 'app/components/static/about.html',
@@ -113,39 +86,18 @@
           controller: 'StaticController',
           controllerAs: 'vm'
         })
-        // .state('layout.kitEdit', {
-        //   url: '/kits/:id/edit?step',
-        //   templateUrl: 'app/components/kit/editKit/editKit.html',
-        //   controller: 'EditKitController',
-        //   controllerAs: 'vm',
-        //   resolve: {
-        //     belongsToUser: belongsToUser,
-        //     redirectNotOwner: redirectNotOwner,
-        //     step: function($stateParams) {
-        //       return parseInt($stateParams.step) || 1;
-        //     }
-        //   }
-        // })
-        // .state('layout.kitUpload', {
-        //   url: '/kits/:id/upload',
-        //   templateUrl: 'app/components/upload/upload.html',
-        //   controller: 'UploadController',
-        //   controllerAs: 'vm',
-        //   resolve: {
-        //     belongsToUser: belongsToUser,
-        //     kit: ['device', 'FullDevice', '$stateParams', function(device, FullDevice, $stateParams) {
-        //       return device.getDevice($stateParams.id)
-        //       .then(kit => new FullDevice(kit));
-        //     }],
-        //     redirectNotOwner: redirectNotOwner
-        //  }
-        // })
-        // .state('layout.kitAdd', {
-        //   url: '/kits/new',
-        //   templateUrl: 'app/components/kit/newKit/newKit.html',
-        //   controller: 'NewKitController',
-        //   controllerAs: 'vm'
-        // })
+        .state('layout.kitEdit', {
+          url: '/kits/:id/edit?step',
+          templateUrl: 'app/components/interstitial/interstitial.html',
+          controller: 'EditKitController',
+          controllerAs: 'vm',
+        })
+        .state('layout.kitUpload', {
+          url: '/kits/:id/upload',
+          templateUrl: 'app/components/interstitial/interstitial.html',
+          controller: 'UploadController',
+          controllerAs: 'vm',
+        })
 
         /*
         -- Home state --
@@ -187,9 +139,6 @@
               controllerAs: 'tagsCtl'
             }
           },
-        //   resolve: {
-        //     belongsToUser:  belongsToUser
-        //  }
         })
         /*
         -- Show Kit state --
@@ -206,9 +155,6 @@
             }
           },
           params: {id: '', reloadMap: false},
-          // resolve: {
-          //   belongsToUser: belongsToUser
-          // }
         })
         /*
         -- User Profile state --
@@ -216,33 +162,35 @@
         Public profile of a given user
         Redirects to My Profile/My Profile Admin if the user is the one authenticated or if the authenticated user is an admin
         */
-        // .state('layout.userProfile', {
-        //   url: '/users/:id',
-        //   templateUrl: 'app/components/userProfile/userProfile.html',
-        //   controller: 'UserProfileController',
-        //   controllerAs: 'vm',
-        //   resolve: {
-        //     isCurrentUser: function($stateParams, $location, auth) {
-        //       if(!auth.isAuth()) {
-        //         return;
-        //       }
-        //       var userID = parseInt($stateParams.id);
-        //       var authUserID = auth.getCurrentUser().data && auth.getCurrentUser().data.id;
-        //       if(userID === authUserID) {
-        //         $location.path('/profile');
-        //       }
-        //     },
-        //     isAdmin: function($window, $location, $stateParams, auth, AuthUser) {
-        //       var userRole = (auth.getCurrentUser().data && auth.getCurrentUser().data.role) || ($window.localStorage.getItem('smartcitizen.data') && new AuthUser(JSON.parse( $window.localStorage.getItem('smartcitizen.data') )).role);
-        //       if(userRole === 'admin') {
-        //         var userID = $stateParams.id;
-        //         $location.path('/profile/' + userID);
-        //       } else {
-        //         return false;
-        //       }
-        //     }
-        //   }
-        // })
+        .state('layout.userProfile', {
+          url: '/users/:id',
+          templateUrl: 'app/components/interstitial/interstitial.html',
+          controller: 'UserProfileController',
+          controllerAs: 'vm',
+          resolve: {
+            isCurrentUser: function($stateParams, $location, auth) {
+              // console.log('iscurrentuser')
+              if(!auth.isAuth()) {
+                return;
+              }
+              var userID = parseInt($stateParams.id);
+              var authUserID = auth.getCurrentUser().data && auth.getCurrentUser().data.id;
+              if(userID === authUserID) {
+                $location.path('/profile');
+              }
+            },
+            isAdmin: function($window, $location, $stateParams, auth, AuthUser) {
+              var userRole = (auth.getCurrentUser().data && auth.getCurrentUser().data.role) || ($window.localStorage.getItem('smartcitizen.data') && new AuthUser(JSON.parse( $window.localStorage.getItem('smartcitizen.data') )).role);
+              // console.log('isAdmin')
+              if(userRole === 'admin') {
+                var userID = $stateParams.id;
+                $location.path('/profile/' + userID);
+              } else {
+                return false;
+              }
+            }
+          }
+        })
         /*
         -- My Profile state --
         Private profile of the authenticated user at the moment
@@ -251,30 +199,42 @@
           url: '/profile',
           authenticate: true,
           abstract: true,
-          templateUrl: 'app/components/myProfile/myProfile.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controller: 'MyProfileController',
           controllerAs: 'vm',
           resolve: {
-            userData: function($location, $window, user, auth, AuthUser) {
-              var userData = auth.getCurrentUser().data;
-              if(!userData) {
-                return;
-              }
+            userData: function(auth) {
+              var userData = auth.getCurrentUserFromAPI()
+              .then(function(data){
+                  return data;
+              })
               return userData;
             }
           }
+        // TODO
+        // I have tried to work on doing a redirect from the resolver, but I think this is not a good practice
+        // I delegate the work on the controller instead, with the interstitial template
         })
         .state('layout.myProfile.kits', {
           url: '/kits',
           cache: false,
           authenticate: true,
-          templateUrl: 'app/components/myProfile/Kits.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controllerAs: 'vm',
+          resolve: {
+            userData: function(auth) {
+              var userData = auth.getCurrentUserFromAPI()
+              .then(function(data){
+                  return data;
+              })
+              return userData;
+            }
+          }
         })
         .state('layout.myProfile.user', {
           url: '/users',
           authenticate: true,
-          templateUrl: 'app/components/myProfile/Users.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controllerAs: 'vm',
         })
         /*
@@ -285,7 +245,7 @@
           url: '/profile/:id',
           authenticate: true,
           abstract: true,
-          templateUrl: 'app/components/myProfile/myProfile.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controller: 'MyProfileController',
           controllerAs: 'vm',
           resolve: {
@@ -310,91 +270,15 @@
           url: '/kits',
           cache: false,
           authenticate: true,
-          templateUrl: 'app/components/myProfile/Kits.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controllerAs: 'vm',
         })
         .state('layout.myProfileAdmin.user', {
           url: '/users',
           authenticate: true,
-          templateUrl: 'app/components/myProfile/Users.html',
+          templateUrl: 'app/components/interstitial/interstitial.html',
           controllerAs: 'vm',
         })
-        /*
-        -- Login --
-        It redirects to a certain kit state and opens the login dialog automatically
-        */
-        // .state('layout.login', {
-        //   url: '/login',
-        //   authenticate: false,
-        //   resolve: {
-        //     buttonToClick: function($location, auth) {
-        //       // TODO: Bug These transitions get rejected (console error)
-        //       if(auth.isAuth()) {
-        //         $location.path('/kits/');
-        //       }else{
-        //         $location.path('/kits/');
-        //         $location.search('login', 'true');
-        //       }
-        //     }
-        //   }
-        // })
-        /*
-        -- Signup --
-        It redirects to a certain kit state and opens the signup dialog automatically
-        */
-        // .state('layout.signup', {
-        //   url: '/signup',
-        //   authenticate: false,
-        //   resolve: {
-        //     buttonToClick: function($location, auth) {
-        //       if(auth.isAuth()) {
-        //         return $location.path('/kits/');
-        //       }
-        //       $location.path('/kits/');
-        //       $location.search('signup', 'true');
-        //     }
-        //   }
-        // })
-        /*
-        -- Logout --
-        It removes all the user data from localstorage and redirects to landing state
-        */
-        // .state('logout', {
-        //   url: '/logout',
-        //   authenticate: true,
-        //   resolve: {
-        //     logout: function($location, $state, auth, $rootScope) {
-        //       auth.logout();
-        //       $location.path('/kits/');
-        //       $rootScope.$broadcast('loggedOut');
-        //     }
-        //   }
-        // })
-        /*
-        -- Password Recovery --
-        Form to input your email address to receive an email to reset your password
-        */
-      //  TODO REMOVE
-        .state('passwordRecovery', {
-          url: '/password_reset',
-          authenticate: false,
-          templateUrl: 'app/components/passwordRecovery/passwordRecovery.html',
-          controller: 'PasswordRecoveryController',
-          controllerAs: 'vm'
-        })
-        /*
-        -- Password Reset --
-        This link will be given by the email you received after giving your email in the previous state
-        Here, you can input your new password
-        */
-      //  TODO REMOVE
-        .state('passwordReset', {
-          url: '/password_reset/:code',
-          authenticate: false,
-          templateUrl: 'app/components/passwordReset/passwordReset.html',
-          controller: 'PasswordResetController',
-          controllerAs: 'vm'
-        });
 
       /*  Disable missing aria-label warnings in console */
       $mdAriaProvider.disableWarnings();
--- src/app/core/api/auth.service.js
+++ src/app/core/api/auth.service.js
@@ -21,11 +21,7 @@
         isAuth: isAuth,
         setCurrentUser: setCurrentUser,
         getCurrentUser: getCurrentUser,
-        // updateUser: updateUser,
-        // saveToken: saveToken,
-        // getToken: getToken,
-        // login: login,
-        // logout: logout,
+        getCurrentUserFromAPI: getCurrentUserFromAPI,
         recoverPassword: recoverPassword,
         getResetPassword: getResetPassword,
         patchResetPassword: patchResetPassword,
@@ -43,30 +39,11 @@
         setCurrentUser('appLoad');
       }
 
-      // Not applicable anymore
-      //run on app initialization so that we can keep auth across different sessions
-      // 1. Check if token in cookie exists. Return if it doesn't, user needs to login (and save a token to the cookie)
-      // 2. Populate user.data with the response from the API.
-      // 3. Broadcast logged in
-
       function setCurrentUser(time) {
-        // TODO later: Should we check if token is expired here?
-        // if (getToken()) {
-        //   vm.user.token = getToken();
-        // }else{
-        //   //console.log('token not found in cookie, returning');
-        //   return;
-        // }
-
-        // console.log('---- SET CURRENT USER -----');
 
         return getCurrentUserFromAPI()
           .then(function(data) {
-            // Save user.data also in localStorage. It is beeing used across the app.
-            // Should it instead just be saved in the user object? Or is it OK to also have it in localStorage?
-            // $window.localStorage.setItem('smartcitizen.data', JSON.stringify(data.plain()) );
-            // console.log('data')
-            // console.log(data);
+
             var newUser = new AuthUser(data);
             //check sensitive information
             if(vm.user.data && vm.user.data.role !== newUser.role) {
@@ -75,9 +52,6 @@
             }
             vm.user.data = newUser;
 
-            //console.log('-- User populated with data: ', user)
-            // Broadcast happens 2x, so the user wont think he is not logged in.
-            // The 2nd broadcast waits 3sec, because f.x. on the /kits/ page, the layout has not loaded when the broadcast is sent
             $rootScope.$broadcast('loggedIn');
 
             // used for app initialization
@@ -97,67 +71,21 @@
           }, errorHandler)
         }
 
-      // Called from device.service.js updateContext(), which is called from multiple /kit/ pages
-      // function updateUser() {
-      //   console.log('update user')
-      //   return getCurrentUserFromAPI()
-      //     .then(function(data) {
-      //       // TODO: Should this update the token or user.data? Then it could instead call setCurrentUser?
-      //       $window.localStorage.setItem('smartcitizen.data', JSON.stringify(data.plain()) );
-      //       return getCurrentUser();
-      //     });
-      // }
-
       function getCurrentUser() {
-        // console.log('vm.user')
-        // console.log(vm.user);
-        // user.token = getToken();
-        // user.data = $window.localStorage.getItem('smartcitizen.data') && new AuthUser(JSON.parse( $window.localStorage.getItem('smartcitizen.data') ));
         return vm.user;
       }
 
       // Should check if user.token exists - but now checks if the cookies.token exists.
       function isAuth() {
-        // TODO: isAuth() is called from many different services BEFORE auth.init has run.
-        // That means that the user.token is EMPTY, meaning isAuth will be false
-        // We can cheat and just check the cookie, but we should NOT. Because auth.init should also check if the cookie is valid / expired
-        // Ideally it should return !!user.token
-        //return !!user.token;
-        // return !!getToken();
-        // updateUser();
 
         if(vm.user.data === undefined){
           return false
         }
 
-        // console.log('user')
-        // console.log(vm.user);
-        // console.log(vm.user.data.key);
-        // console.log(!!vm.user.data.key)
         return !!vm.user.data.key
       }
 
-      // LoginModal calls this after it receives the token from the API, and wants to save it in a cookie.
-      // function saveToken(token) {
-      //   //console.log('saving Token to cookie:', token);
-      //   $cookies.put('smartcitizen.token', token);
-      //   setCurrentUser();
-      // }
-
-      // function getToken(){
-      //   return $cookies.get('smartcitizen.token');
-      // }
-
-      // function login(loginData) {
-      //   return Restangular.all('sessions').post(loginData);
-      // }
-
-      // function logout() {
-      //   $cookies.remove('smartcitizen.token');
-      // }
-
       function getCurrentUserFromAPI() {
-        // console.log('getting user')
         return Restangular.all('').customGET('me');
       }
 
